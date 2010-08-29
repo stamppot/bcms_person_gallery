08b547d19acadc31ba650ceca7e18d96df3f602e
Jens Rasmussen <stamppot@gmail.com>
2010-08-29 18:45:13 +0200

    order column name changed to position

--- app/controllers/cms/person_types_controller.rb
+++ app/controllers/cms/person_types_controller.rb
@@ -1,7 +1,7 @@
 class Cms::PersonTypesController < Cms::ContentBlockController
 
   def create
-    max_item = (PersonType.maximum(:order) || "0").to_i
+    max_item = (PersonType.maximum(:position) || 0)
     params[:person_type][:position] = (max_item + 1).to_s
     super
   end
